c1edbde79d66b796d1a83d0ee9d8078681ee10d1
Patrick <revmydev@gmail.com>
2019-02-06 14:26:56 -0800

    fixed previous commit; version bump; cleanup

--- AbsoluteHit.js
+++ AbsoluteHit.js
@@ -2,7 +2,7 @@
 	@file AbsoluteHit.js
 	@see https://bitbucket.org/pstrube/AbsoluteHit.git
 	@author pstrube
-	@version 1.1
+	@version 1.2
 
 	TODO:
 	fix lag in safari -- may not be possible because apple?
@@ -11,48 +11,33 @@
 const HIT_TIME = 100;
 const PLUGIN_ROOT = getPluginRoot();
 
-window.onload = function() {
-	var abshitAudio = document.createElement('AUDIO');
-	var imgPath48 = PLUGIN_ROOT + "assets/v/marker_48.png";
-	var imgPath32 = PLUGIN_ROOT + "assets/v/marker_32.png";
+var abshitAudio = document.createElement('AUDIO');
+var imgPath48 = PLUGIN_ROOT + "assets/v/marker_48.png";
+var imgPath32 = PLUGIN_ROOT + "assets/v/marker_32.png";
 
-	abshitAudio.id = "abshit";
-	abshitAudio.src = PLUGIN_ROOT + "assets/a/hitmarker.mp3";
-	abshitAudio.preload = "auto";
-	document.body.appendChild(abshitAudio);
+abshitAudio.id = "abshit";
+abshitAudio.src = PLUGIN_ROOT + "assets/a/hitmarker.mp3";
+abshitAudio.preload = "auto";
+document.body.appendChild(abshitAudio);
 
-	// HEY! LISTEN!
+// HEY! LISTEN!
 
-	window.document.addEventListener("mousedown", event => {
-		let oldAnchorStyle;
-		let anchorHasStyling = false;
-		let anchorTriggered = false;
+window.document.addEventListener("mousedown", event => {
+	let oldAnchorStyle;
 
-		if(event.target.nodeName == "A" || event.target.nodeName == "BUTTON"){
-			document.getElementsByTagName('html')[0].style.cursor = "url(" + imgPath32 + "), auto";
-			anchorTriggered = true;
-			if(event.target.style.cursor != ""){
-				anchorHasStyling = true;
-				oldAnchorStyle = event.target.style.cursor;
-			}
-			event.target.style.cursor = "url(" + imgPath32 + "), auto";
-
-			abshitAudio.play(); 
+	if(event.target.nodeName == "A" || event.target.nodeName == "BUTTON"){
+		oldAnchorStyle = event.target.style.cursor;
+		event.target.style.cursor = "url(" + imgPath32 + "), auto";
+		document.getElementsByTagName('html')[0].style.cursor = "url(" + imgPath32 + "), auto";
 		
-			setTimeout(function(){
-				document.getElementsByTagName('html')[0].style.cursor = "";
-				
-				if(anchorTriggered){
-					if(anchorHasStyling){
-						event.target.style.cursor = oldAnchorStyle;
-					} else {
-						event.target.style.cursor = "";
-					}
-				}
-			}, HIT_TIME);
-		}
-	});
-};
+		abshitAudio.play(); 
+	
+		setTimeout(function(){
+			event.target.style.cursor = oldAnchorStyle;
+			document.getElementsByTagName('html')[0].style.cursor = "";
+		}, HIT_TIME);
+	}
+});
 
 
 // helper
