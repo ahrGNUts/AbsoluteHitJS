1cfc9ddba23c4c5a490e7ed0f8a6541e8fa13d3d
Patrick <revmydev@gmail.com>
2019-02-06 11:40:32 -0800

    generalized getCursorPath(); wrapped all logic in window.onload function; fixed audio asset not loading on some installs

--- AbsoluteHit.js
+++ AbsoluteHit.js
@@ -2,43 +2,70 @@
 	@file AbsoluteHit.js
 	@see https://bitbucket.org/pstrube/AbsoluteHit.git
 	@author pstrube
+	@version 1.1
+
+	TODO:
+	fix lag in safari -- may not be possible because apple?
 */
 
-var abshitAudio = document.createElement('AUDIO');
-var imgPath48 = getCursorPath(48);
-var imgPath32 = getCursorPath(32);
+const HIT_TIME = 100;
+const PLUGIN_ROOT = getPluginRoot();
 
-abshitAudio.id = "abshit";
-abshitAudio.src = "assets/a/hitmarker.mp3";
-abshitAudio.preload = "auto";
-document.body.appendChild(abshitAudio);
+window.onload = function() {
+	var abshitAudio = document.createElement('AUDIO');
+	var imgPath48 = PLUGIN_ROOT + "assets/v/marker_48.png";
+	var imgPath32 = PLUGIN_ROOT + "assets/v/marker_32.png";
 
-// HEY! LISTEN!
+	abshitAudio.id = "abshit";
+	abshitAudio.src = PLUGIN_ROOT + "assets/a/hitmarker.mp3";
+	abshitAudio.preload = "auto";
+	document.body.appendChild(abshitAudio);
 
-window.document.addEventListener("mousedown", function() {
-	document.getElementsByTagName('html')[0].style.cursor = "url(" + imgPath32 + "), auto";
-	
-	abshitAudio.play(); 
-	
-	setTimeout(function(){
-		document.getElementsByTagName('html')[0].style.cursor = "auto";
-	}, 100);
-});
+	// HEY! LISTEN!
+
+	window.document.addEventListener("mousedown", event => {
+		let oldAnchorStyle;
+		let anchorHasStyling = false;
+		let anchorTriggered = false;
+
+		if(event.target.nodeName == "A" || event.target.nodeName == "BUTTON"){
+			document.getElementsByTagName('html')[0].style.cursor = "url(" + imgPath32 + "), auto";
+			anchorTriggered = true;
+			if(event.target.style.cursor != ""){
+				anchorHasStyling = true;
+				oldAnchorStyle = event.target.style.cursor;
+			}
+			event.target.style.cursor = "url(" + imgPath32 + "), auto";
+
+			abshitAudio.play(); 
+		
+			setTimeout(function(){
+				document.getElementsByTagName('html')[0].style.cursor = "";
+				
+				if(anchorTriggered){
+					if(anchorHasStyling){
+						event.target.style.cursor = oldAnchorStyle;
+					} else {
+						event.target.style.cursor = "";
+					}
+				}
+			}, HIT_TIME);
+		}
+	});
+};
+
 
 // helper
-function getCursorPath(suffix) {
+function getPluginRoot() {
 	var scripts = document.getElementsByTagName("script");
 	var path;
 
-	for(var i = 0; i < scripts.length; i++){
+	for(let i = 0; i < scripts.length; i++){
 		if(scripts[i].src.includes("AbsoluteHit.js")){
 			path = scripts[i].src;
-			break;
+			
+			let lastSlash = path.lastIndexOf('/');
+			return path.slice(0, lastSlash+1);
 		}
 	}
-
-	var lastSlash = path.lastIndexOf('/');
-	path = path.slice(0, lastSlash+1);
-
-	return path + "assets/v/marker_" + suffix + ".png";
 }
